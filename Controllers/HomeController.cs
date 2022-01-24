66580b21a58936d0e89206004cc59f97842d17bd
mohammadkaif11 <mohammadkaif0211@gmail.com>
2022-01-24 19:34:18 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is index";
+            return View();
+        }
+
+        public ActionResult Sport()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Entertainment()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Technology()
+        {
+            return View();
+        }
+    }
+}
